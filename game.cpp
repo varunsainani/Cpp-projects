19992a9bd3bca33c5f326af8319b35082f5b6ee8
Varun Sainani <varunsainani0990@gmail.com>
2024-05-05 22:15:09 +0500

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(void)
+{
+    int hostUserNum, guestUserNum;
+    cout << "Host: ";
+    cin >> hostUserNum;
+    system("clear");
+    cout<<"you have 3 attempts "<<endl;
+    
+
+    int i=1;
+    do
+    {
+        cout << "Guest: ";
+        cin >> guestUserNum;
+        i++;
+        
+    }while (hostUserNum != guestUserNum && i <= 3);
+
+    if (hostUserNum == guestUserNum)
+    {
+        cout<<"correct !"<<endl;
+    }
+    else{
+        cout<<"failed!"<<endl;
+    }
+    
+    return 0;
+}
